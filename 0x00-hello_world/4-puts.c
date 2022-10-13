8d472625f17cd643f0eed1285695c2b8e90eaba9
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-10-13 22:12:40 +0000

    Update 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /** 
- * main: prints "Programming is like building a multilingual puzzle
- * return: 0 if successful, non-zero otherwise
+ *main: prints "Programming is like building a multilingual puzzle
+ *return: 0 if successful, non-zero otherwise
  */
 int main(void)
 {

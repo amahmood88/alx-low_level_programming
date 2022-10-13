0237cc7e979af3ac15cac9a514a39688b441178e
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-10-13 15:50:29 -0700

    Update 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-
-/* main function prints phrase Programming is like building a multilingual puzzle */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	/* returns 0 if execution is successful, non-zero otherwise */	
+	puts("\"Programming is like building a multilingual puzzle");	
 	return (0);
 }

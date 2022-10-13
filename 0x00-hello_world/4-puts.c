e90fe1c835e5fb456659976fb6248eb8457a709f
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-10-13 15:36:52 -0700

    Update 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/* main function prints phrase "Programming is like building a multilingual puzzle */
+/* main function prints phrase Programming is like building a multilingual puzzle */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

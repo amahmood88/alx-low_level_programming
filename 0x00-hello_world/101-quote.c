722f0c4fd38030659c6bac8460ffe49441f41062
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-10-13 16:46:50 -0700

    Create 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - prints out a phrase to stderr
+ * return: 0 if successful, non-zero otherwise
+ */
+int main(void)
+{
+	printf(stderr, "and that piece of art is useful" - Dora Korper, \
+			2015 - 10 - 19);
+	return (0);
+}

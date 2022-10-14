03c2c2c81f66e23f0276875dd0ecdb27a8c01e3f
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-10-13 17:13:42 -0700

    Update 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
- *main - prints out a phrase to stderr
- * return: ensures a return value of 1
- */
+*main - entry point to use printf.
+*return: ensures main returns a value of 1.
+*/
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korper, 2015-10-19\n");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }

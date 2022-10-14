9aedf00952c5577a5f345e3cfa286a7da3ea6761
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-10-14 22:51:20 +0000

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: 0 if successful, !0 otherwise
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c < 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar("\n");
+	return (0);
+}
+

05c6bcefadb2ecc46a8255eca52ad2c990d76812
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-10-18 22:01:02 +0000

    Update comment in 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,7 +3,7 @@
 /**
  * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
  * followed by a new line
- * Return: Always returns 0 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {

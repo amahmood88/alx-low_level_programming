00f9824364f8b2c2e245087e90336e0001f5f592
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-10-18 21:17:37 +0000

    Create function that prints numbers up to 98 based on input value

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  *print_to_98 - print all natural numbers to 98
+  *@n: single parameter of function
+  *Return: always returns 0
+  */
+
+void print_to_98(int n)
+{
+	if (n < 0
+  

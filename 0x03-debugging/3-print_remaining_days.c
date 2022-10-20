ffe752ea956038b659da9dab824af5dffc02796f
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-10-20 23:04:25 +0000

    Update print_remaining_days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
-* print_remaining_days - takes a date and prints the remaining days
+* print_remaining_days - takes a date and prints how many days are
 * left in the year, taking leap years into account
-* @month: month of year
-* @day: Day of the month
+* @month: month of the year
+* @day: Day of month
 * @year: year
 */
 

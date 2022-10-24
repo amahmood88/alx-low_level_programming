af3eb513d1c3b7f1968fab7029a1e42cc8b748ba
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-10-24 22:07:02 +0000

    Update 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * rev_string - reverses a string
+  * @s: pointer to a string
+  */
+void rev_string(char *s)
+{
+	char tmp;
+	int i, len, len_2;
+
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len_2 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len_2];
+		s[len_2--] = tmp;
+	}
+}

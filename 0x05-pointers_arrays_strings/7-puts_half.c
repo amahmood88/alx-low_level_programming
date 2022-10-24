924ffac7f24979313070c4b788ed27772a17ca47
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-10-24 22:02:30 +0000

    Update function to print upper half of characters if len_of_string is odd

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,17 +6,27 @@
   */
 void puts_half(char *str)
 {
-	int i, len, n;
+	int len, n, i;
 
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	len = i + 1;
-	if ((len % 2) != 0)
-		n = ((len - 1) / 2);
-	for (i = n; i <= len; i++)
+	len = 0;
+
+	while (str[len] != '\0')
 	{
-		_putchar(str[i]);
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
 	}
 	_putchar('\n');
 }

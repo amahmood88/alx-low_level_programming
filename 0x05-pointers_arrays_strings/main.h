f048ed23244000f18fb3348779f40c26917cde76
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-10-24 19:51:33 +0000

    Create function to reverse a string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,10 +2,13 @@
 #define _MAIN_H_
 
 #include <stdio.h>
+#include <string.h>
 
 
 void reset_to_98(int *n);
 void swap_int(int *x, int *y);
 int _strlen(char *s);
+void _putchar(char c);
+void print_rev(char *s);
 
 #endif /* _MAIN_H_ */

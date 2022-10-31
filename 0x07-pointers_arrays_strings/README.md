e6f73e0fa49d57abed09edfd9b22462ffbee659f
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-10-31 20:39:03 +0000

    Create README.md and function files to fill memory with constant byte

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This is yet another repo that contains pointers, arratrings.
+The essence is to better understand how to use them to write better code.
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+
+
+void simple_print_buffer(char *buffer, unsigned int size);
+char *_memset(char *s, char b, unsigned int n);
+
+
+#endif /* _MAIN_H_ */

271a6362bba7a35fe50f90518ee1c1ef5fac0a54
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-11-01 20:43:48 +0000

    Create 3-strspn.c to get the length of substring and update main.h

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  * _strspn - gets the length of a prefix substring
+  * @s: intialization string
+  * @accept: gives bytes
+  * Return: accepted bytes
+  */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, k;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j = 1;
+		for (k = 0; accept[k] != '\0'; k++)
+		{
+			if (s[k] ==  accept[k])
+			{
+				j = 0;
+				break;
+			}
+		}
+		if (j == 1)
+		{
+			break;
+		}
+	}
+	return (i);
+}

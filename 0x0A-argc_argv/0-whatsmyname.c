d34385a0a061f66eb8d9eb69481859014c40b88c
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-11-05 08:11:12 +0000

    Create a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * main - entry point
+  * @argc: number of input arguments
+  * @argv: name of input elements of vector argument
+  * Return: 0 always
+  */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}

3b075a0b785d714753fa4cc82fa15c429856144e
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-11-07 17:39:52 +0000

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
   * @argv: name of input elements of vector argument
   * Return: 0 always
   */
-int main((void) int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);

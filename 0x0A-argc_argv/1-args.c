25955889cb86bd51a71b45a2c7a5f5e2810e9c75
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-11-07 14:28:41 +0000

    Create function that prints number of input arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * main - entry point
+  * @argc: number of input arguments
+  * @argv: string vector of input arguments
+  * Return: 0 always
+  */
+int main(int argc, int *argv[])
+{
+	printf("%d\n", (argc - 1));
+	return (0);
+}
+
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 
 int _putchar(char c);
+int main(int argc, int *argv[]);
 
 #endif /* _MAIN_H_ */

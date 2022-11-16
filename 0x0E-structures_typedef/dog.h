28d034b1fb16b6114588675f1ca895f09038f61c
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-11-16 05:26:26 +0000

    Create a type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+  * struct dog - create a structure with three properties
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: owner of dog
+  *
+  * Description: contains the bio data of a dog
+  */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

859f61badd070fe34ff3333474b537263bc03ace
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-11-17 09:58:06 +0000

    Create function that prints a name; README.md; function_pointers.h

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,9 @@
+/**
+  * print_name - function that prints a name
+  * @name: pointer to a char
+  * @f: pointer to a function
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef _FUNC_PTR_H_
+#define _FUNC_PTR_H_
+
+void print_name(char *name, void (*f)(char *));
+
+#endif /* _FUNC_PTR_H_ */
